7bf63c6ba1b2a5203037dd2b61669841e8bc4768
wil2106 <serviantwilliam@gmail.com>
2020-02-06 22:32:08 +0100

    commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('wesh').end();
+  res.status(200).send('wesheuu').end();
 });
 
 module.exports = app
