323a36ece99211ee402ab661929e1dd9013e4816
wil2106 <serviantwilliam@gmail.com>
2020-02-06 21:54:27 +0100

    commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('bonjour').end();
+  res.status(200).send('wesh').end();
 });
 
 module.exports = app
--- app.test.js
+++ app.test.js
@@ -8,6 +8,6 @@ describe('App', () => {
     });
     test('should return Hello World!', async () => {
         const response = await request(app).get('/');
-        expect(response.text).toBe('bonjour');
+        expect(response.text).toBe('wesh');
     });
 })
