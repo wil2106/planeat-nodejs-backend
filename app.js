b259b845f8cbb5d62d03082049acf9058bbcbc39
wil2106 <serviantwilliam@gmail.com>
2020-02-07 20:45:17 +0100

    commit

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 const express = require('express');
-//var mysql = require('mysql');
+var mysql = require('mysql');
 
 const app = express();
-/*
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "yourusername",
-  password: "yourpassword",
-  database:""
-});
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
-*/
+
+let pool;
+const createPool = async () => {
+  pool = await mysql.createPool({
+    user: process.env.DB_USER, // e.g. 'my-db-user'
+    password: process.env.DB_PASS, // e.g. 'my-db-password'
+    database: process.env.DB_NAME, // e.g. 'my-database'
+    socketPath: `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`
+  });
+};
+createPool();
+
+var test = function(req, res) {
+  pool.getConnection(function(err, conn){
+      conn.query("select * from Recipes", function(err, rows) {
+           res.json(rows);
+      })
+  })
+}
+
+
 
 app.get('/', (req, res) => {
   res.status(200).send('Bonjour').end();
