e1b2e6f0ff4acca2f0c63fdff1586d40985e7896
wil2106 <serviantwilliam@gmail.com>
2020-02-07 09:52:39 +0100

    commit

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
-const app = express();
+var mysql = require('mysql');
 
+const app = express();
+/*
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword",
+  database:""
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+*/
 app.get('/', (req, res) => {
-  res.status(200).send('wesheuu').end();
+  res.status(200).send('salut').end();
 });
 
 module.exports = app
