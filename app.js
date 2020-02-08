9fa126e65d0d096202ee45c31f084296873ca2b5
wil2106 <serviantwilliam@gmail.com>
2020-02-08 17:54:50 +0100

    comit

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
 const express = require('express');
 var mysql = require('mysql');
+var app = express();
 
-const app = express();
+
+var pool = mysql.createPool({
+  connectionLimit : 10, //important
+  user: process.env.DB_USER, // e.g. 'my-db-user'
+  password: process.env.DB_PASS, // e.g. 'my-db-password'
+  database: process.env.DB_NAME, // e.g. 'my-database'
+  socketPath: `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
+  debug    :  false
+});
+
+function handle_database(req,res) {
+  pool.getConnection(function(err,connection){
+      if (err) {
+        res.json({"code" : 100, "status" : "Error in connection database"});
+        return;
+      }  
+
+      console.log('connected as id ' + connection.threadId);
+     
+      connection.query("select * from Recipes",function(err,rows){
+          connection.release();
+          if(!err) {
+              res.json(rows);
+          }          
+      });
+
+      connection.on('error', function(err) {      
+            res.json({"code" : 100, "status" : "Error in connection database"});
+            return;    
+      });
+});
+}
+
+/*
 
 let pool;
 const createPool = async () => {
@@ -11,17 +45,18 @@ const createPool = async () => {
     database: process.env.DB_NAME, // e.g. 'my-database'
     socketPath: `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`
   });
-  console.log(pool);
+  
 };
 
 createPool();
 
-
+*/
 
 
 
 
 app.get('/', (req, res) => {
+  handle_database(req,res);
   res.status(200).send('Bonjour').end();
 });
 
